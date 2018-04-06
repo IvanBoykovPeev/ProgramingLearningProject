b404b4aa9f642f4ead31e0fb916ecc40903342f9
IvanBoykovPeev <ivan.boykov.peev@gmail.com>
2018-04-06 14:37:09 +0300

    Add lab 03EmployeeData

---
+++ 03EmployeeData/EmployeeData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03EmployeeData
+{
+    class EmployeeData
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            int employeeID = int.Parse(Console.ReadLine());
+            double mounthlySalary = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Name: {0}", name);
+            Console.WriteLine("Age: {0}", age);
+            Console.WriteLine("Employee ID: {0:D8}", employeeID);
+            Console.WriteLine("Salary: {0:F2}", mounthlySalary);
+        }
+    }
+}
