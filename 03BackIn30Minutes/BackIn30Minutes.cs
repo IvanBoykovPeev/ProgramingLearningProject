4c58498f6c39ab157517a4ef6d4ae9a0c41e0208
IvanBoykovPeev <ivan.boykov.peev@gmail.com>
2018-04-09 10:12:25 +0300

    Add some lab solved

---
+++ 03BackIn30Minutes/BackIn30Minutes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _03BackIn30Minutes
+{
+    class BackIn30Minutes
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            minutes += 30;
+
+            if (minutes > 59)
+            {
+                hours++;
+                if (hours > 23)
+                {
+                    hours = 0;
+                    minutes -= 60;
+                }
+                else
+                {
+                    minutes -= 60;
+                }
+            }
+            Console.WriteLine($"{hours}:{minutes:D2}");
+        }
+    }
+}
